022a52293bf9741c3db72e34ee5459aa73a9e17b
Atakan Ozceviz <atakanozceviz@hotmail.com>
2022-05-31 14:45:35 +0200

    use pointer receiver

--- internal/core/transaction.go
+++ internal/core/transaction.go
@@ -20,7 +20,7 @@ type Transaction struct {
 	metadata             map[string]string
 }
 
-func (t Transaction) ID() ID {
+func (t *Transaction) ID() ID {
 	return t.id
 }
 
@@ -28,31 +28,31 @@ func (t *Transaction) SetId(id ID) {
 	t.id = id
 }
 
-func (t Transaction) PaymentTransactionID() string {
+func (t *Transaction) PaymentTransactionID() string {
 	return t.paymentTransactionID
 }
 
-func (t Transaction) Amount() uint64 {
+func (t *Transaction) Amount() uint64 {
 	return t.amount
 }
 
-func (t Transaction) Address() *Address {
+func (t *Transaction) Address() *Address {
 	return t.address
 }
 
-func (t Transaction) PaymentMethodType() payment_method_type.PaymentMethodType {
+func (t *Transaction) PaymentMethodType() payment_method_type.PaymentMethodType {
 	return t.paymentMethodType
 }
 
-func (t Transaction) Action() transaction_action.TransactionAction {
+func (t *Transaction) Action() transaction_action.TransactionAction {
 	return t.action
 }
 
-func (t Transaction) Status() transaction_status.TransactionStatus {
+func (t *Transaction) Status() transaction_status.TransactionStatus {
 	return t.status
 }
 
-func (t Transaction) Metadata() map[string]string {
+func (t *Transaction) Metadata() map[string]string {
 	return t.metadata
 }
 
@@ -84,19 +84,19 @@ type Address struct {
 	postCode     string
 }
 
-func (a Address) City() string {
+func (a *Address) City() string {
 	return a.city
 }
 
-func (a Address) Street() string {
+func (a *Address) Street() string {
 	return a.street
 }
 
-func (a Address) StreetNumber() string {
+func (a *Address) StreetNumber() string {
 	return a.streetNumber
 }
 
-func (a Address) PostCode() string {
+func (a *Address) PostCode() string {
 	return a.postCode
 }
 
